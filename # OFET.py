5db7ed93a3aab2680de19234693bdc08eef655fd
alicealchaar <alicealchaar@hotmail.com>
2024-05-27 20:00:25 -0300

    Update # OFET.py

--- # OFET.py
+++ # OFET.py
@@ -5,7 +5,7 @@ import os
 from tkinter import filedialog
 from scipy.optimize import curve_fit
 
-tempo,vds,vgs,ids,vgs,potência,v_inicial,v_final = [None]*8
+tempo,vds,vgs,ids,vgs,potência,v_inicial,v_final,x0,a,coef_saturação = [None]*11
 abs_ids,sqrt_ids,vgs_intervalo,ids_intervalo = [],[],[],[]
 
 def colunas_extras(ids):
@@ -14,6 +14,7 @@ def colunas_extras(ids):
         abs_ids.append(abs(i))
         sqrt_ids.append(np.sqrt(abs(i)))
     plt.plot(vgs,sqrt_ids)
+    #plt.plot(vds,sqrt_ids, label = 'VDS')
     plt.xlabel('VGS')
     plt.ylabel('sqrt(IDS)')
     plt.show(block=False)
@@ -22,15 +23,15 @@ def reta(x,a,b):
     return a * x + b
 
 def ler_arquivo_txt(caminho_arquivo):
-    global tempo,vds,vgs,ids,igs,vgs,potência, v_inicial,v_final, vgs_intervalo,ids_intervalo
+    global tempo,vds,vgs,ids,igs,vgs,potência, v_inicial,v_final, vgs_intervalo,ids_intervalo,x0,a, coef_saturação
     nome_arquivo = os.path.splitext(os.path.basename(caminho_arquivo))[0]
     with open(caminho_arquivo, 'r') as arquivo:
         linhas = arquivo.readlines()
-        tempo = []
-        vds = []
+        #tempo = []
+        #vds = []
         vgs = []
         ids = []
-        igs = []
+        #igs = []
         #potência = []
         armazenar = False 
 
@@ -42,15 +43,18 @@ def ler_arquivo_txt(caminho_arquivo):
                 if linha[0].isalpha() or linha[0].isspace():
                     break  
                 colunas = linha.split()
-                tempo.append(float(colunas[0]))
-                vds.append (float(colunas[1]))
-                vgs.append(float(colunas[2]))
-                ids.append(float(colunas[3]))
-                igs.append(float(colunas[4]))
+                #tempo.append(float(colunas[0]))
+                #vds.append (float(colunas[1]))
+                vgs.append(float(colunas[0]))
+                ids.append(float(colunas[1]))
+                #igs.append(float(colunas[4]))
                 #potência.append(float(colunas[5]))
     colunas_extras(ids)
-    v_inicial=int(input("Digite o valor inicial da tensão da parte da curva onde começa a reta: "))
-    v_final = int(input("Agora, o valor final: "))
+    v_inicial=float(input("Digite o valor inicial da tensão da parte da curva onde começa a reta: "))
+    v_final = float(input("Agora, o valor final: "))
+    Ci = float(input("Digite o valor da capacitância específica do dielétrico(μF/m^2): "))*(10**-10)
+    L = float(input("Digite o comprimento do canal(μm): "))
+    W = float(input("Digite a largura do canal(μm): "))
     plt.close()
     for pos,i in enumerate(vgs):
         if pos>0:
@@ -64,24 +68,32 @@ def ler_arquivo_txt(caminho_arquivo):
     coef, incerteza = curve_fit(reta, vgs_intervalo, ids_intervalo)
     a, b = coef
     x0 = -b/a
-    plt.plot(vgs_intervalo, ids_intervalo, 'o', label='Dados experimentais',markersize=5)
+    coef_saturação = (2*L*a*a)/(W*Ci)
+    plt.plot(vgs,sqrt_ids)
+    #plt.plot(vgs_intervalo, ids_intervalo, 'o', label='Dados experimentais',markersize=5)
     plt.plot(vgs_intervalo, reta(vgs_intervalo, a, b), 'r-', label=f'Fitting')
     plt.xlabel('VGS')
     plt.ylabel('sqrt(IDS)')
     plt.legend()
     plt.show()
-    print(f'O coeficiente angular é {a}')
+    print(f'O coeficiente é {coef_saturação}')
+    print(f'slope {a}')
     print(f'O valor de x para y=0 é {x0}')
 
-def salvar_dados():
-    with open(caminho_arquivo, 'w') as file:
-        file.write("Tempo  VDS VGS  IDS  IGS |IDS| sqrt(|IDS|)\n")
-        for i in range(len(tempo)):
-            file.write("{:.7e}  {:.7e}  {:.7e}  {:.7e}  {:.7e} {:.7e}  {:.7e}\n".format(tempo[i], vds[i], vgs[i],ids[i],igs[i],abs_ids[i], sqrt_ids[i]))
-        # print ("Os dados foram salvos com sucesso")
+# def salvar_dados():
+#     with open(caminho_arquivo, 'w') as file:
+#         file.write("Tempo            VDS              VGS              IDS              IGS         |IDS|         sqrt(|IDS|)    V_limiar    coef_angular  mobilidade\n")
+#         for i in range(len(tempo)):
+#             if i == 0:
+#                 file.write("{:.7e}  {:.7e}  {:.7e}  {:.7e}  {:.7e} {:.7e}  {:.7e}    {:.7e}  {:.7e}       {:.7e}\n".format(
+#                     tempo[i], vds[i], vgs[i], ids[i], igs[i], abs_ids[i], sqrt_ids[i], x0, a, coef_saturação))
+#             else:
+#                 file.write("{:.7e}  {:.7e}  {:.7e}  {:.7e}  {:.7e} {:.7e}  {:.7e}    \n".format(
+#                     tempo[i], vds[i], vgs[i], ids[i], igs[i], abs_ids[i], sqrt_ids[i]))
 
-#caminho_arquivo = 'C:/Users/Estudante/Desktop/LOEM/Alice/OFET/Disp1/30um medida1 transf -40V.txt'
-caminho_arquivo = 'C:/Users/alice/Documents/IC/OFET/30um medida1 transf -40V.txt'
+# caminho_arquivo = 'C:/Users/Estudante/Desktop/LOEM/Alice/OFET/Disp1/30um medida1 transf -40V.txt'
+caminho_arquivo = 'C:/Users/Estudante/Desktop/LOEM/Alice/OFET/ex.txt'
+# caminho_arquivo = 'C:/Users/alice/Documents/IC/OFET/30um medida1 transf -40V.txt'
 ler_arquivo_txt(caminho_arquivo)
-salvar_dados()
+#salvar_dados()
 
