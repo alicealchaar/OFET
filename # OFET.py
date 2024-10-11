709cf4b199e00e0cc6f860072ee79e8728df899b
alicealchaar <alicealchaar@hotmail.com>
2024-10-11 18:32:22 -0300

    Update # OFET.py

--- # OFET.py
+++ # OFET.py
@@ -8,38 +8,18 @@ from scipy.optimize import curve_fit
 import glob
 import re
 
-tempo,vds,vgs,ids,vgs,potência,v_inicial,v_final,x0_ida,x0_volta,a,coef_saturação = [None]*12
-abs_ids,sqrt_ids,vgs_intervalo_ida,ids_intervalo_ida,vgs_intervalo_volta,ids_intervalo_volta, v_limiar_ida,v_limiar_volta, medidas, ids_leitura_ida, ids_leitura_volta= [],[],[],[],[],[],[],[],[],[],[]
+vgs_ida,ids_ida,vgs_volta,ids_volta,ciclos = [],[],[],[],[]
 
-def colunas_extras(ids):
-    global abs_ids,sqrt_ids
-    abs_ids,sqrt_ids =[],[]
-    for i in ids:
-        abs_ids.append(abs(i))
-        sqrt_ids.append(np.sqrt(abs(i)))
-    # plt.plot(vgs,sqrt_ids)
-    # plt.plot(vds,sqrt_ids, label = 'VDS')
-    # plt.xlabel('VGS')
-    # plt.ylabel('sqrt(IDS)')
-    # plt.show(block=False)
-
-def reta(x,a,b):
-    return a * x + b
 
 def ler_arquivo_txt(caminho_arquivo,L,W,Ci,j):
-    global tempo,vds,vgs,ids,igs,vgs,potência, v_inicial,v_final, vgs_intervalo_ida,ids_intervalo_ida, vgs_intervalo_volta,ids_intervalo_volta,x0_ida,x0_volta,a, coef_saturação, v_limiar_ida, v_limiar_volta, ids_leitura_ida, ids_leitura_volta
-    vgs_intervalo_volta=[]
-    ids_intervalo_volta=[]
-    vgs_intervalo_ida = []
-    ids_intervalo_ida = []
+    global vgs_ida,ids_ida,vgs_volta,ids_volta,ciclos
     with open(caminho_arquivo, 'r') as arquivo:
         linhas = arquivo.readlines()
-        tempo = []
-        vds = []
-        vgs = []
-        ids = []
-        igs = []
-        potência = []
+        vgs_ida = []
+        ids_ida = []
+        vgs_volta = []
+        ids_volta = []
+        ciclos = []
         armazenar = False 
 
         for linha in linhas:
@@ -50,72 +30,13 @@ def ler_arquivo_txt(caminho_arquivo,L,W,Ci,j):
                 if linha[0].isalpha() or linha[0].isspace():
                     break  
                 colunas = linha.split()
-                tempo.append(float(colunas[0]))
-                vds.append (float(colunas[1]))
-                vgs.append(float(colunas[2]))
-                ids.append(float(colunas[3]))
-                igs.append(float(colunas[4]))
-                potência.append(float(colunas[5]))
-    colunas_extras(ids)
-    # v_inicial=float(input("Digite o valor inicial da tensão da parte da curva onde começa a reta: "))
-    # v_final = float(input("Agora, o valor final: "))
-    v_inicial = int(-17)       ##########Lembrar de ajeitar esses valores
-    v_final = int(-20)
-    depois_da_curva = None
-    for pos,i in enumerate(vgs): #pos = index
-        if pos>0:
-            if i>vgs[pos-1]:
-                depois_da_curva = 'ok'
-            if depois_da_curva == None:
-                if i<=v_inicial and i>=v_final:
-                    vgs_intervalo_ida.append(i)
-                    ids_intervalo_ida.append(sqrt_ids[pos])
-            else:
-                if i<=v_inicial and i>=v_final:
-                    vgs_intervalo_volta.append(i)
-                    ids_intervalo_volta.append(sqrt_ids[pos])    
-    #ida           
-    vgs_intervalo_ida = np.array(vgs_intervalo_ida)
-    ids_intervalo_ida = np.array(ids_intervalo_ida)
-    coef_ida, incerteza_ida = curve_fit(reta, vgs_intervalo_ida, ids_intervalo_ida)
-    a_ida, b_ida = coef_ida
-    x0_ida = -b_ida/a_ida
-    # coef_saturação = (2*L*a*a)/(W*Ci)
-    v_limiar_ida.append(x0_ida) 
-     #volta
-    vgs_intervalo_volta = np.array(vgs_intervalo_volta)
-    ids_intervalo_volta = np.array(ids_intervalo_volta)
-    coef_volta, incerteza_volta = curve_fit(reta, vgs_intervalo_volta, ids_intervalo_volta)
-    a_volta, b_volta = coef_volta
-    x0_volta = -b_volta/a_volta
-    # coef_saturação = (2*L*a*a)/(W*Ci)
-    v_limiar_volta.append(x0_volta) 
+                vgs_ida.append(float(colunas[0]))
+                ids_ida.append (float(colunas[1]))
+                vgs_volta.append(float(colunas[2]))
+                ids_volta.append(float(colunas[3]))
+                ciclos.append(float(colunas[4]))
 
-    i_depois_da_curva = None
-    i15_ida = ids[0]
-    i15_volta = ids[0]
-    i15_ida_indice = 0
-    i15_volta_indice = 0
-
-    #plt.plot(vgs,sqrt_ids, color = 'red')
-
-    for pos, i in enumerate(vgs):
-        if pos != 0:
-            if i>vgs[pos-1]:
-                i_depois_da_curva = 'ok'
-            if i_depois_da_curva == None:
-                if abs(vgs[pos]+15)<abs(vgs[i15_ida_indice]+15):
-                    i15_ida = ids[pos]
-                    i15_ida_indice = pos
-            else:
-                if abs(vgs[pos]+15)<abs(vgs[i15_volta_indice]+15):
-                    i15_volta = ids[pos]
-                    i15_volta_indice = pos 
-    
-    ids_leitura_ida.append(i15_ida)
-    ids_leitura_volta.append(i15_volta)
-
-pasta = 'C:/Users/Estudante/Desktop/LOEM/Alice/OFET/24 08 12 (férias)/Disp 2/80um/80um 24 08 08 (-40V) perfeito'
+pasta = 'C:/Users/Estudante/Desktop/LOEM/Alice/OFET/24 10 04 Alice e Harold - disp 1/60um'
 nome_pasta= os.path.basename(pasta)
 match = re.search(r'\d+',nome_pasta) #para pegar o comprimento do canal a partir do nome da pasta
 L = int(match.group())
@@ -124,36 +45,43 @@ Ci = 50
 arquivos = glob.glob(os.path.join(pasta,"*.txt")) #para ler cada cada arquivo .txt dentro da pasta
 for j,caminho_arquivo in enumerate (arquivos):
     nome_arquivo = os.path.basename(caminho_arquivo) 
-    medidas.append(j)
     ler_arquivo_txt(caminho_arquivo,L,W,Ci,j)
-    plt.plot(vgs,sqrt_ids)
-    plt.xlabel("VGS")
-    plt.ylabel("sqrt(IDS)")
-plt.show()
 
-# plt.plot(medidas,v_limiar_ida,marker = 'o', color = 'blue')
-# plt.plot(medidas,v_limiar_volta,marker = 'o', color = 'red')
-# plt.xlabel('Medidas')
-# plt.ylabel('V_limiar')
-# plt.show()
-
-plt.plot(medidas,ids_leitura_ida,marker = 'o', color = 'blue')
-plt.plot(medidas,ids_leitura_volta,marker = 'o', color = 'red')
-plt.xlabel('Medidas')
+#plotar tudo junto (as 4 curvas)
+plt.scatter(ciclos,ids_ida,marker = 'o', color = 'blue',label = "IDA")
+plt.scatter(ciclos,ids_volta,marker = 'o', color = 'red', label = "VOLTA" )
+plt.legend()
+plt.xlabel('Ciclos')
 plt.ylabel('IDS')
 plt.show()
 
-# plt.plot(medidas,v_limiar_todos,marker = 'o', color = 'blue')
-# plt.xlabel('Medidas')
-# plt.ylabel('V_limiar')
-# plt.show()
+medida_par_ida = [] #leitura depois de escrito
+medida_par_volta = [] 
+medida_impar_ida = [] #Leitura depois de apagado
+medida_impar_volta = [] 
+ciclos_meio = []
 
-# # def criar_arquivo(diretorio):
-# #     nome = "_DadosTemporais.txt"
-# #     caminho = os.path.join(diretorio, nome)
-# #     with open(caminho, 'w') as file:
-# #         for i in range (len(medidas)):
-# #             file.write("{:.7e}  {:.7e}\n".format(medidas[i],v_limiar_todos[i]))
+for i,el in enumerate (ids_ida):
+    if (i%2==0):
+        medida_par_ida.append(ids_ida[i])
+        medida_par_volta.append(ids_volta[i])
+        ciclos_meio.append(ciclos[i])
+    else:
+        medida_impar_ida.append(ids_ida[i])
+        medida_impar_volta.append(ids_volta[i])
 
-# # criar_arquivo(pasta)
-# # print("Os dados foram salvos com sucesso!")
+plt.scatter(ciclos_meio,medida_par_volta,marker = 'o', color = 'blue',label = "LEITURA ESCRITO")
+plt.scatter(ciclos_meio,medida_impar_volta,marker = 'o', color = 'red', label = "LEITURA APAGADO" )
+plt.legend()
+plt.xlabel('Ciclos')
+plt.ylabel('IDS')
+plt.title("IDS VOLTA")
+plt.show()
+
+plt.scatter(ciclos_meio,medida_par_ida,marker = 'o', color = 'blue',label = "LEITURA ESCRITO")
+plt.scatter(ciclos_meio,medida_impar_ida,marker = 'o', color = 'red', label = "LEITURA APAGADO" )
+plt.legend()
+plt.xlabel('Ciclos')
+plt.ylabel('IDS')
+plt.title("IDS IDA")
+plt.show()
